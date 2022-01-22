30de854eb8422dc229fc40450a81d06094216fc3
f74169 <f74169@gmail.com>
2022-01-22 15:29:14 +0800

    月亮太陽轉換

---
+++ Assets/hundo1018/Scripts/TimeEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.hundo1018.Scripts
+{
+    public class TimeEventArgs
+    {
+        public float RemainingTime { get; set; }
+        public float RemainingRate { get; set; }
+        public bool IsNight { get; set; }
+        public TimeEventArgs(float remainingTime,float remainingRate)
+        {
+            RemainingTime = remainingTime;
+            RemainingRate = remainingRate;
+        }
+        public TimeEventArgs(bool isNight)
+        {
+            IsNight = isNight;
+        }
+    }
+}
