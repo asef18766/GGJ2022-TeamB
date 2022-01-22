7e6b73e060396990213dcb4e304e6fee3af53057
unknown <asef18766@gmail.com>
2022-01-22 15:30:21 +0800

    finish wolf and sound system

--- Assets/asef18766/Scripts/Wolf/WolfManager.cs
+++ Assets/asef18766/Scripts/Wolf/WolfManager.cs
@@ -13,19 +13,21 @@ namespace asef18766.Scripts.Wolf
         
         private static WolfManager _instance = null;
         private readonly Vector2[] _spawnBounds= {
-            new Vector2(0,0), // left up
-            new Vector2(0,0), // right down
+            new Vector2(-4,4), // left right
+            new Vector2(-5,5), // down up
         };
 
-        public GameObject playerRef = null;
+        public GameObject PlayerRef = null;
 
-        private int respawnCount = 3;
+        private int _respawnCount = 3;
         
         #endregion
 
         private WolfManager()
         {
-            _wolf = Resources.Load<GameObject>("Assets/asef18766/Prefabs/player.prefab");
+            _wolf = Resources.Load<GameObject>("Prefabs/Player");
+            if (_wolf == null)
+                throw new ApplicationException("can not obtain player prefab");
         }
 
         public static WolfManager GetInstance()
@@ -38,8 +40,8 @@ namespace asef18766.Scripts.Wolf
             // searching for nice location until wall does not exists
             while (true)
             {
-                var x = Random.Range(_spawnBounds[0].x, _spawnBounds[1].x);
-                var y = Random.Range(_spawnBounds[0].y, _spawnBounds[1].y);
+                var x = Random.Range(_spawnBounds[0].x, _spawnBounds[0].y);
+                var y = Random.Range(_spawnBounds[1].x, _spawnBounds[1].y);
 
                 Debug.DrawRay(new Vector2(x,y), Vector2.one, Color.yellow);
                 Debug.Log("try search spawn location");
@@ -54,14 +56,15 @@ namespace asef18766.Scripts.Wolf
         public void SpawnWolf()
         {
             var loc = RespawnLoc();
-            playerRef = Object.Instantiate(_wolf, loc, Quaternion.identity);
+            Debug.LogWarning($"try to spawn wolf at ({loc.x}, {loc.y})");
+            PlayerRef = Object.Instantiate(_wolf, loc, Quaternion.identity);
         }
 
         public void KillWolf()
         {
-            respawnCount--;
+            _respawnCount--;
             Object.Destroy(_wolf);
-            if (respawnCount == 0)
+            if (_respawnCount == 0)
             {
                 Debug.LogWarning("game over, player failed");
                 throw new NotImplementedException();    
