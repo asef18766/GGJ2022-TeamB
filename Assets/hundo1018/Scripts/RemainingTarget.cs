b4a02d9b0df4920a2aa7e2a6d842dc5e44d98da1
f74169 <f74169@gmail.com>
2022-01-22 18:23:50 +0800

    剩餘屠殺目標數量加上浮誇特效

--- Assets/hundo1018/Scripts/RemainingTarget.cs
+++ Assets/hundo1018/Scripts/RemainingTarget.cs
@@ -5,11 +5,47 @@ using UnityEngine.UI;
 
 public class RemainingTarget : MonoBehaviour
 {
-    private Text _text;
-    const string ALREADY = "已屠殺";
-    const string TARGET = "目標";
-    private void UpdateText(int alreadyKillAmount, int targetAmount)
+    [SerializeField] private Animator textAnimation;
+    [SerializeField]private Text _text;
+    [SerializeField] bool TempPlus;
+    //private const string ALREADY = "已屠殺";
+    //private const string TARGET ="目標";
+    [SerializeField] private int _currentAmount;
+    [SerializeField] private int _targetAmount;
+    /// <summary>
+    /// 直接設定
+    /// </summary>
+    /// <param name="currenAmount"></param>
+    /// <param name="targetAmount"></param>
+    public void UpdateText(int currenAmount, int targetAmount)
     {
-        _text.text = $"{ALREADY} {alreadyKillAmount}/{TARGET} {targetAmount}";
+        _currentAmount = currenAmount;
+        _targetAmount = targetAmount;
+        if (_currentAmount >= _targetAmount) _currentAmount = _targetAmount;
+        _text.text = $"{_currentAmount}/{_targetAmount}";
+        textAnimation.Play("Shake");
+    }
+    /// <summary>
+    /// 增加數量
+    /// </summary>
+    public void Add()
+    {
+        UpdateText(_currentAmount + 1, _targetAmount);
+    }
+    /// <summary>
+    /// 重設當前數量
+    /// </summary>
+    public void ResetCurrent()
+    {
+        _currentAmount = 0;
+    }
+
+    private void Update()
+    {
+        if (TempPlus)
+        {
+            TempPlus = false;
+            Add();
+        }
     }
 }
