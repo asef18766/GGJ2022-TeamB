bd5b543b8d09e3c288f997eaaf2868bdd115fc0a
unknown <asef18766@gmail.com>
2022-01-23 12:22:17 +0800

    finish wolf animation

--- Assets/asef18766/Scripts/Wolf/AnimationController.cs
+++ Assets/asef18766/Scripts/Wolf/AnimationController.cs
@@ -21,41 +21,70 @@ namespace asef18766.Scripts.Wolf
 
         private Wolf _wolf = null;
         private SpriteRenderer _spriteRenderer = null;
-        private static readonly int FaceDir = Animator.StringToHash("face_dir");
-        private static readonly int IsMoving = Animator.StringToHash("is_moving");
-        private static readonly int WolfMode = Animator.StringToHash("wolf_mode");
 
         private void Start()
         {
             _animator = GetComponent<Animator>();
+            _animator.Play(_currentAnimationState);
             _wolf = WolfManager.GetInstance().PlayerRef.GetComponent<Wolf>();
             _spriteRenderer = GetComponent<SpriteRenderer>();
+
+            _wolf.WolfAttack = o =>
+            {
+                if (_currentAnimationState.Contains("side"))
+                    _animator.Play("attack_side");
+                else if (_currentAnimationState.Contains("front"))
+                    _animator.Play("attack_front");
+                else if (_currentAnimationState.Contains("back"))
+                    _animator.Play("attack_back");
+            };
+            _wolf.StartWolfMode.Add(o =>
+            {
+                _animator.Play("transform");
+            });
+            _wolf.EndWolfMode = o =>
+            {
+                _animator.Play("rev_transform");
+            };
         }
 
         public void UpdateDirection(Vector2 dir)
         {
-            if (dir != Vector2.zero)
+            var nState = "";
+            if (dir == Vector2.left)
             {
-                if (dir == Vector2.left)
-                {
-                    _animator.SetInteger(FaceDir, 2);
-                    _spriteRenderer.flipX = false;
-                }
-                else if (dir == Vector2.right)
-                {
-                    _animator.SetInteger(FaceDir, 2);
-                    _spriteRenderer.flipX = true;
-                }
-                else if (dir.y < 0)
-                    _animator.SetInteger(FaceDir, 0);
-                else
-                    _animator.SetInteger(FaceDir, 1);
-                _animator.SetBool(IsMoving, true);
+                nState = "walk_side";
+                _spriteRenderer.flipX = false;
             }
+            else if (dir == Vector2.right)
+            {
+                nState = "walk_side";
+                _spriteRenderer.flipX = true;
+            }
+            else if (dir.y < 0)
+                nState = "walk_front";
+            else if (dir.y > 0)
+                nState = "walk_back";
             else
-                _animator.SetBool(IsMoving, false);
+            {
+                var substring = _currentAnimationState.Substring(4, _currentAnimationState.Length - 4);
+                nState = "idle" + substring;
+                Debug.Log(nState);
+            }
             
-            _animator.SetBool(WolfMode, _wolf.WolfMode);
+            if (nState == _currentAnimationState)
+                return;
+            
+            if (_wolf.WolfMode && (nState[nState.Length -1] != '0'))
+                nState += " 0";
+
+            _currentAnimationState = nState;
+            _animator.Play(_currentAnimationState);
+        }
+
+        private void OnDestroy()
+        {
+            _animator.Play("respawn");
         }
     }
 }
